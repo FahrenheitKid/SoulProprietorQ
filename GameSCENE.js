51068a8c4a155c4336be941afbd7e7d5c12bd4b7
Jordan Silva <jordan.silvaa42@gmail.com>
2016-04-13 22:18:11 -0300

    Random color and tint

--- GameSCENE.js
+++ GameSCENE.js
@@ -1,45 +1,57 @@
- var game = new Phaser.Game(800, 600, Phaser.AUTO, { preload: preload, create: create, update: update });
 
 
- var model;
+var game = new Phaser.Game(1280, 720, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update });
+
+var model;
  var background;
+ var color;
  var dragPosition;
+var bmd;
+var text;
 
  function preload()
  {
 
- 	game.load.image('background', 'assets/sprites/background.png');
- 	game.load.image('model', 'assets/sprites/bosskid.png');
+    game.load.image('background', 'assets/sprites/background.jpg');
+    game.load.image('model', 'assets/sprites/char00.png');
 
  }
+function create() {
 
- function create()
- {
+  background = game.add.sprite(0,0,'background');
+    model = game.add.sprite(100, 100, 'model');
+  
 
- 	//Arcade Physics system
- 	//game.physics.startSystem(Phaser.Physics.ARCADE);
+    model.inputEnabled = true;
+    model.input.enableDrag();
 
+    model.events.onInputOver.add(onOver, this);
+    model.events.onInputOut.add(onOut, this);
+    model.events.onDragStart.add(onDragStart, this);
 
- 	model = game.add.sprite(100, 100, 'model');
- 	background = game.add.sprite(0,0,'background');
- 	model.inputEnabled = true;
- 	model.input.enableDrag();
+    
+    model.events.onDragStop.add(onDragStop, this);
 
- 	model.events.onInputOver.add(onOver, this);
- 	model.events.onInputOut.add(onOut, this);
- 	model.events.onDragStart.add(onDragStart, this);
- 	model.events.onDragStop.add(onDragStop, this);
+    dragPosition = new Phaser.Point(model.x, model.y);
+    color = randomColor({hue: "yellow", luminosity: "light"});
+    color = parseInt(color.substr(1), 16);
 
- 	dragPosition = new Phaser.Point(model.x, model.y);
- }
+    bmd = game.add.bitmapData(game.width, game.height);
 
- function update()
- {
- 	//Collision
- 	//game.physics.arcade.overlap(bullets, enemies, collisionHandler, null, this);
- }
+    //  Black and opaque
+    //bmd.fill(0, 0, 0, 1);
 
- function collisionHandler(bullet, enemy)
+    bmd.addToWorld();
+
+    // debugzin
+    text = game.make.text(0, 0, color, { font: "bold 32px Arial", fill: "#ff0044" });
+    text.anchor.set(0.5);
+
+    bmd.draw(text, game.world.randomX, game.world.randomY);
+
+}
+
+function collisionHandler(bullet, enemy)
  {
 
  }
@@ -53,39 +65,42 @@
  function onOver(sprite, pointer)
  {
 
- 	sprite.tint = 0xff7777;
+    sprite.tint = color;
 
  }
 
  function onOut(sprite, pointer)
  {
 
- 	sprite.tint = 0xffffff;
+    sprite.tint = 0xffffff;
 
  }
 
  function onDragStart(sprite, pointer)
  {
 
- 	dragPosition.set(sprite.x, sprite.y);
+    dragPosition.set(sprite.x, sprite.y);
 
  }
 
  function onDragStop(sprite, pointer)
  {
 
- 	/*
- 	 	if (!sprite.overlap(dropZone))
- 	 	{
- 	 		game.add.tween(sprite).to(
- 	 		{
- 	 			x: dragPosition.x,
- 	 			y: dragPosition.y
- 	 		}, 500, "Back.easeOut", true);
- 	 	}
- 	*/
+    /*
+        if (!sprite.overlap(dropZone))
+        {
+            game.add.tween(sprite).to(
+            {
+                x: dragPosition.x,
+                y: dragPosition.y
+            }, 500, "Back.easeOut", true);
+        }
+    */
  }
 
  function render () {
+
+    game.debug.text( "This is debug text: ", 100, 380 );
+    game.debug.spriteBounds(model);
 
 }
